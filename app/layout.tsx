55e35478163255ef019b54e61c750ce0db3e82d9
vmatseyv <vmatsyev@gmail.com>
2025-10-16 09:17:03 +0300

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         />
         <Script 
           type="text/javascript" 
-          src="https://app.comagic.ru/static/cs.min.js?k=XmZcX1deUQbZo5Vl5mAI60C9P9ZgKGnt" 
+          src="https://app.comagic.ru/static/cs.min.js?k=OvKXDWSlorAc7kQq8IE41zoGUxIiZIcm" 
         />
         <Script id="gtm" strategy="afterInteractive">
           {`
@@ -49,6 +49,23 @@ export default function RootLayout({
           type="text/javascript" 
           src="https://smartcallback.ru/api/SmartCallBack.js?t=kUp43RtwmYtq46zaqKrtyh" 
         />
+        <Script id="ym-init" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=95223539', 'ym');
+
+            ym(95223539, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/95223539" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+          </div>
+        </noscript>
       </head>
       <body>
         {children}
